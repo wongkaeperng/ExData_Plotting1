f49cca7b3accf780d9e8eb20cb446e8747fca9c1
wongkaeperng <wongkaeperng@hotmail.com>
2014-07-10 12:14:41 +0800

    Plot 1 Code

---
+++ plot1.R
@@ -0,0 +1,34 @@
+#Load Data 
+loadPowerData <- function() {
+      data <- read.table("household_power_consumption.txt",
+                       header=TRUE,
+                       sep=";",
+                       colClasses=c("character", "character", rep("numeric",7)),
+                       na="?")
+      #Convert time variable
+      data$Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+      
+      # Convert date variable
+      data$Date <- as.Date(data$Date, "%d/%m/%Y")
+      
+      # Extract data with 2 dates
+      dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+      data <- subset(data, Date %in% dates)
+      
+      return(data)
+}
+
+#Plot the data
+plot1 <- function(data=NULL) {
+      data <- loadPowerData()
+      
+      png("plot1.png", width=600, height=600)
+      
+      hist(data$Global_active_power,
+           main="Global Active Power",
+           xlab="Global Active Power (kilowatts)",
+           ylab="Frequency",
+           col="red")
+      
+      dev.off()
+}
